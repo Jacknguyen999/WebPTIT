f48cfe66f788476ce141dc54ce518513cd9c44ea
Jacknguyen999 <88345014+Jacknguyen999@users.noreply.github.com>
2024-03-01 09:58:11 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const passwordField = document.getElementById("password");
+const togglePassword = document.getElementById("show-pwd-btn");
+
+togglePassword.addEventListener("click", function () {
+    console.log(passwordField.type);
+if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+} else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+}
+});
